d27e271dabfa2d214faf9d47c112ed12dd9c942d
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2022-11-10 22:39:04 +0900

    Update progress_indicators_page.dart

--- mi_boilerplates/example/lib/pages/progress_indicators_page.dart
+++ mi_boilerplates/example/lib/pages/progress_indicators_page.dart
@@ -21,85 +21,137 @@ class ProgressIndicatorsPage extends ConsumerWidget {
 
   static final _logger = Logger((ProgressIndicatorsPage).toString());
 
+  static const _tabs = <Widget>[
+    MiTab(text: 'Determinate'),
+    MiTab(text: 'Indeterminate'),
+  ];
+
   const ProgressIndicatorsPage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    //final enableActions = ref.watch(enableActionsProvider);
+    _logger.fine('[i] build');
 
+    final enabled = ref.watch(enableActionsProvider);
+
+    return MiDefaultTabController(
+      length: _tabs.length,
+      initialIndex: 0,
+      builder: (context) {
+        return Scaffold(
+          appBar: ExAppBar(
+            prominent: ref.watch(prominentProvider),
+            icon: icon,
+            title: title,
+            bottom: ExTabBar(
+              enabled: enabled,
+              tabs: _tabs,
+            ),
+          ),
+          body: const SafeArea(
+            minimum: EdgeInsets.all(8),
+            child: TabBarView(
+              children: [
+                _DeterminateTab(),
+                _IndeterminateTab(),
+              ],
+            ),
+          ),
+          bottomNavigationBar: const ExBottomNavigationBar(),
+        );
+      },
+    ).also((_) {
+      _logger.fine('[o] build');
+    });
+  }
+}
+
+class _DeterminateTab extends ConsumerWidget {
+  // ignore: unused_field
+  static final _logger = Logger((_DeterminateTab).toString());
+
+  const _DeterminateTab();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
 
-    return Scaffold(
-      appBar: ExAppBar(
-        prominent: ref.watch(prominentProvider),
-        icon: icon,
-        title: title,
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 40,
+        vertical: 8,
       ),
-      body: SafeArea(
-        minimum: const EdgeInsets.only(
-          left: 40,
-          top: 12,
-          right: 40,
-          bottom: 8,
+      child: MiAnimationController(
+        duration: const Duration(seconds: 4),
+        onInitialized: (controller) {
+          controller.forward();
+        },
+        onCompleted: (controller) {
+          controller.reset();
+          controller.forward();
+        },
+        builder: (_, controller, __) => AnimatedBuilder(
+          animation: controller,
+          builder: (_, __) {
+            final t = math.min(controller.value * 2.0, 1.0);
+            return Column(
+              children: [
+                SizedBox(
+                  height: kToolbarHeight,
+                  child: Center(
+                    child: CircularProgressIndicator(
+                      value: t,
+                      backgroundColor: theme.backgroundColor,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 24,
+                  child: Center(
+                    child: LinearProgressIndicator(
+                      value: t,
+                      backgroundColor: theme.backgroundColor,
+                    ),
+                  ),
+                ),
+              ],
+            );
+          },
         ),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: kToolbarHeight,
-                child: Center(
-                  child: CircularProgressIndicator(),
-                ),
-              ),
-              const SizedBox(
-                height: kToolbarHeight,
-                child: Center(
-                  child: LinearProgressIndicator(),
-                ),
-              ),
-              MiAnimationController(
-                duration: const Duration(seconds: 4),
-                onInitialized: (controller) {
-                  controller.forward();
-                },
-                onCompleted: (controller) {
-                  controller.reset();
-                  controller.forward();
-                },
-                builder: (_, controller, __) => AnimatedBuilder(
-                  animation: controller,
-                  builder: (_, __) {
-                    final t = controller.value;
-                    return Column(
-                      children: [
-                        SizedBox(
-                          height: kToolbarHeight,
-                          child: Center(
-                            child: CircularProgressIndicator(
-                              value: math.min(t * 2, 1.0),
-                              backgroundColor: theme.backgroundColor,
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: kToolbarHeight,
-                          child: Center(
-                            child: LinearProgressIndicator(
-                              value: math.min(t * 2, 1.0),
-                              backgroundColor: theme.backgroundColor,
-                            ),
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                ),
-              ),
-            ],
+      ),
+    );
+  }
+}
+
+class _IndeterminateTab extends ConsumerWidget {
+  // ignore: unused_field
+  static final _logger = Logger((_IndeterminateTab).toString());
+
+  const _IndeterminateTab();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 40,
+        vertical: 8,
+      ),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: kToolbarHeight,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
           ),
-        ),
+          SizedBox(
+            height: kToolbarHeight * 0.5,
+            child: Center(
+              child: LinearProgressIndicator(),
+            ),
+          ),
+        ],
       ),
-      bottomNavigationBar: const ExBottomNavigationBar(),
     );
   }
 }
