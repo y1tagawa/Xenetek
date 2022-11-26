2a3bcb6e3b8c2e333589cb7634096b170716770f
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2022-11-26 17:16:14 +0900

    add svg repo license.

--- mi_boilerplates/example/lib/main.dart
+++ mi_boilerplates/example/lib/main.dart
@@ -7,6 +7,7 @@ import 'dart:developer';
 
 import 'package:collection/collection.dart';
 import 'package:example/pages/page_layouts_page.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -301,6 +302,23 @@ void main() async {
   Logger.root.level = Level.ALL;
   Logger.root.onRecord.listen((record) {
     log('${record.level.name}: ${record.time}: ${record.loggerName}: ${record.message}');
+  });
+
+  LicenseRegistry.addLicense(() {
+    return Stream<LicenseEntry>.fromIterable(<LicenseEntry>[
+      const LicenseEntryWithLineBreaks(<String>['svg-repo-svg.svg'], '''
+Public Domain\n
+https://www.svgrepo.com/page/licensing/\n\n
+The person who associated a work with this deed has dedicated the work to the public domain by waiving all of his or her rights to the work worldwide under copyright law, including all related and neighboring rights, to the extent allowed by law.
+Or, the work consists of simple geometry and is not ineligable for copyright due to Threshold of originality (this threshold might vary depending on different country laws). For an example "A stick figure, where the head is represented by a circle and other parts represented by straight lines" is not copyrightable or falls into public domain.
+You are free:\n
+* to share – to copy, distribute and transmit the work\n
+* to remix – to adapt the work\n
+Under the following terms:\n
+* attribution – there is no author or author waived their right, no attribution\n
+* share alike – If you remix, transform, or build upon the material, you can distribute your work under any license.\n
+    '''),
+    ]);
   });
 
   runApp(const ProviderScope(child: MyApp()));
