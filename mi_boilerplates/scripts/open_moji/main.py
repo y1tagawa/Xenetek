5cde69bde2de1b0c2436532b22a3beaae7839016
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2022-11-27 21:33:09 +0900

    update

--- mi_boilerplates/scripts/open_moji/main.py
+++ mi_boilerplates/scripts/open_moji/main.py
@@ -24,11 +24,7 @@ def main():
                             534, 535, 536, 537, 538, 539,
         540, 541, 542, 543, 544, 545, 546, 547, 548, 549,
         550, 551, 552, 553,           556, 557, 558, 559,
-        560, 561, 562, 563, 564, 565, 566, 567, 568, 569,
-        570, 571, 572, 573, 574, 575, 576, 577, 578, 579,
-        580, 581, 582, 583, 584, 585, 586, 587, 588, 589,
-        590, 591, 592, 593, 594, 595, 596, 597, 598, 599,
-        600, 601, 602, 603, 604, 605, 606, 607, 608, 609,
+        range(560, 610),
         610, 611, 612, 613, 614, 615, 616, 617,
              621, 622, 623, 624, 625, 626, 627, 628, 629,
         630, 631, 632, 633, 634, 635, 636, 637, 638, 639,
@@ -40,11 +36,15 @@ def main():
     ]
 
     # item list.
-
     items = []
     for index in indices:
-        item = data[str(index)]
-        items.append(item)
+        if isinstance(index, int):
+            items.append(data[str(index)])
+        elif isinstance(index, range):
+            for i in index:
+                items.append(data[str(i)])
+        else:
+            print('invalid index:' + str(index))
 
     # add extra items.
     items.append({'name':'donkey', 'code':'E001', 'keywords':'extra-openmoji'})
@@ -59,7 +59,6 @@ def main():
     items.append({'name':'microbe2', 'code':'E011', 'keywords':'extra-openmoji'})
 
     # generate downloader.'
-
     with open('download.sh', 'w', encoding='utf-8', newline='\n') as f:
         f.write('#!/bin/sh\n')
         f.write('mkdir -p assets/open_moji\n')
@@ -69,14 +68,12 @@ def main():
              fileName + ' 2>&1 | tee -a download.log\n')
 
     # generate .yaml fragment,
-
     with open('fragment.yaml', 'w', encoding='utf-8', newline='\n') as f:
         for item in items:
             fileName = item['code'].upper() + '.svg'
             f.write('  - assets/open_moji/' + fileName + '\n')
 
     # generate importer.
-
     with open('open_moji_svgs.dart.txt', 'w', encoding='utf-8', newline='\n') as f:
         f.write('import \'package:flutter_svg/flutter_svg.dart\';\n\n')
         for item in items:
