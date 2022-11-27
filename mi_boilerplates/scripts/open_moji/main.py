cb1779c396a0381564b10cb45dfeb13019bf48fc
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2022-11-27 21:07:58 +0900

    update open moji

--- mi_boilerplates/scripts/open_moji/main.py
+++ mi_boilerplates/scripts/open_moji/main.py
@@ -12,14 +12,14 @@ def main():
     with open('table.json', 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    # target index list.
+    # table index list.
     indices = [
         # crown, ghost
         109, 112,
         # monkey faces
         125, 126, 127,
         # people
-        350, 354, 358, 378, 380, 390, 395, 399,
+        231, 350, 354, 358, 378, 380, 390, 395, 399, 490,
         # animals
                             534, 535, 536, 537, 538, 539,
         540, 541, 542, 543, 544, 545, 546, 547, 548, 549,
@@ -35,24 +35,43 @@ def main():
         640,      642, 643, 644, 645, 646, 647, 648, 649,
         650, 651, 652, 653, 654, 655, 656, 657,
                        773, 774, 775, 776, 777,
+        # objects
+        818, 1319, 1327
     ]
 
-    # generate downloader.
+    # item list.
+
+    items = []
+    for index in indices:
+        item = data[str(index)]
+        items.append(item)
+
+    # add extra items.
+    items.append({'name':'donkey', 'code':'E001', 'keywords':'extra-openmoji'})
+    items.append({'name':'doe', 'code':'E003', 'keywords':'extra-openmoji'})
+    items.append({'name':'spouting-orca', 'code':'E004', 'keywords':'extra-openmoji'})
+    items.append({'name':'orca', 'code':'E005', 'keywords':'extra-openmoji'})
+    items.append({'name':'narwhal', 'code':'E006', 'keywords':'extra-openmoji'})
+    items.append({'name':'beluga', 'code':'E007', 'keywords':'extra-openmoji'})
+    items.append({'name':'porpoise', 'code':'E008', 'keywords':'extra-openmoji'})
+    items.append({'name':'pigeon', 'code':'E009', 'keywords':'extra-openmoji'})
+    items.append({'name':'macaw', 'code':'E010', 'keywords':'extra-openmoji'})
+    items.append({'name':'microbe2', 'code':'E011', 'keywords':'extra-openmoji'})
+
+    # generate downloader.'
 
     with open('download.sh', 'w', encoding='utf-8', newline='\n') as f:
         f.write('#!/bin/sh\n')
         f.write('mkdir -p assets/open_moji\n')
-        for index in indices:
-            item = data[str(index)]
+        for item in items:
             fileName = item['code'].upper() + '.svg'
             f.write('wget -nc -O assets/open_moji/' + fileName + ' https://openmoji.org/data/color/svg/' +
-             fileName + ' 2>>download.log\n')
+             fileName + ' 2>&1 | tee -a download.log\n')
 
     # generate .yaml fragment,
 
     with open('fragment.yaml', 'w', encoding='utf-8', newline='\n') as f:
-        for index in indices:
-            item = data[str(index)]
+        for item in items:
             fileName = item['code'].upper() + '.svg'
             f.write('  - assets/open_moji/' + fileName + '\n')
 
@@ -60,11 +79,10 @@ def main():
 
     with open('open_moji_svgs.dart.txt', 'w', encoding='utf-8', newline='\n') as f:
         f.write('import \'package:flutter_svg/flutter_svg.dart\';\n\n')
-        for index in indices:
-            item = data[str(index)]
+        for item in items:
             fileName = item['code'].upper() + '.svg'
             name = 'openMojiSvg' + \
-                item['name'].replace('&amp;', 'And') \
+                item['name'].replace('&amp;', ' And ').replace('&gt;', ' Gt ').replace('&lt;', ' Lt ')  \
                     .title().replace(' ', '').replace('-', '') 
             f.write('// ' + item['name'] + ' ' + item['keywords'] + '\n')
             f.write('final ' + name + ' = SvgPicture.asset(\'assets/open_moji/' + fileName + '\');\n')
