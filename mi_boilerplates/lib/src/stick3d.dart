4db5a8849a1185a7bae46baad44f4a77b2f51ac9
y1tagawa <46841556+y1tagawa@users.noreply.github.com>
2023-01-07 20:26:12 +0900

    Update stick3d.dart

--- mi_boilerplates/lib/src/stick3d.dart
+++ mi_boilerplates/lib/src/stick3d.dart
@@ -8,7 +8,14 @@
 
 // https://api.flutter.dev/flutter/vector_math/vector_math-library.html
 
-import 'package:vector_math/vector_math.dart';
+import 'package:flutter/foundation.dart';
+import 'package:vector_math/vector_math_64.dart';
+
+final x_ = Vector3(1, 0, 0);
+final y_ = Vector3(0, 1, 0);
+final z_ = Vector3(0, 0, 1);
+
+// Node
 
 class Node {
   Matrix4? _matrix;
@@ -21,21 +28,26 @@ class Node {
     this.children = const <String, Node>{},
   }) : _matrix = matrix;
 
-  /// [path]に対応する子ノードを検索する。
-  Node? find({
-    required Iterable<String> path,
-  }) {
+  MapEntry<Node, Matrix4>? _find(
+    Iterable<String> path,
+    Matrix4 matrix,
+  ) {
     if (path.isEmpty) {
-      return this;
+      return MapEntry(this, matrix);
     }
     final child = children[path.first];
     if (child == null) {
       return null;
     }
-    return child.find(path: path.skip(1));
+    return child._find(path.skip(1), this.matrix * matrix);
   }
 
-  ///
+  MapEntry<Node, Matrix4>? find(
+    Iterable<String> path,
+  ) {
+    return _find(path, Matrix4.identity());
+  }
+
   Node copy() {
     return Node(
       matrix: _matrix,
@@ -90,16 +102,19 @@ class Node {
 //</editor-fold>
 }
 
+// Mesh data
+
 class MeshVertex {
   int vertexIndex;
-  int normalIndex;
   int textureVertexIndex;
+  int normalIndex;
 
-  MeshVertex({
-    this.vertexIndex = -1,
-    this.normalIndex = -1,
-    this.textureVertexIndex = -1,
-  });
+  MeshVertex(
+    this.vertexIndex,
+    int? textureVertexIndex,
+    int? normalIndex,
+  )   : textureVertexIndex = textureVertexIndex ?? -1,
+        normalIndex = normalIndex ?? -1;
 
   //<editor-fold desc="Data Methods">
 
@@ -109,46 +124,46 @@ class MeshVertex {
       (other is MeshVertex &&
           runtimeType == other.runtimeType &&
           vertexIndex == other.vertexIndex &&
-          normalIndex == other.normalIndex &&
-          textureVertexIndex == other.textureVertexIndex);
+          textureVertexIndex == other.textureVertexIndex &&
+          normalIndex == other.normalIndex);
 
   @override
-  int get hashCode => vertexIndex.hashCode ^ normalIndex.hashCode ^ textureVertexIndex.hashCode;
+  int get hashCode => vertexIndex.hashCode ^ textureVertexIndex.hashCode ^ normalIndex.hashCode;
 
   @override
   String toString() {
     return 'MeshVertex{'
         ' vertexIndex: $vertexIndex,'
-        ' normalIndex: $normalIndex,'
         ' textureVertexIndex: $textureVertexIndex,'
+        ' normalIndex: $normalIndex,'
         '}';
   }
 
   MeshVertex copyWith({
     int? vertexIndex,
-    int? normalIndex,
     int? textureVertexIndex,
+    int? normalIndex,
   }) {
     return MeshVertex(
-      vertexIndex: vertexIndex ?? this.vertexIndex,
-      normalIndex: normalIndex ?? this.normalIndex,
-      textureVertexIndex: textureVertexIndex ?? this.textureVertexIndex,
+      vertexIndex ?? this.vertexIndex,
+      textureVertexIndex ?? this.textureVertexIndex,
+      normalIndex ?? this.normalIndex,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
       'vertexIndex': vertexIndex,
-      'normalIndex': normalIndex,
       'textureVertexIndex': textureVertexIndex,
+      'normalIndex': normalIndex,
     };
   }
 
   factory MeshVertex.fromMap(Map<String, dynamic> map) {
     return MeshVertex(
-      vertexIndex: map['vertexIndex'] as int,
-      normalIndex: map['normalIndex'] as int,
-      textureVertexIndex: map['textureVertexIndex'] as int,
+      map['vertexIndex'] as int,
+      map['textureVertexIndex'] as int,
+      map['normalIndex'] as int,
     );
   }
 
@@ -158,12 +173,18 @@ class MeshVertex {
 typedef MeshFace = List<MeshVertex>;
 
 class MeshData {
-  final List<Vector3> vertices;
-  final List<MeshFace> faces;
+  List<Vector3> vertices;
+  List<Vector3> normals;
+  List<Vector3> textureVertices;
+  List<MeshFace> faces;
+  bool smooth;
 
   MeshData({
     this.vertices = const <Vector3>[],
+    this.normals = const <Vector3>[],
+    this.textureVertices = const <Vector3>[],
     this.faces = const <MeshFace>[],
+    this.smooth = false,
   });
 
 //<editor-fold desc="Data Methods">
@@ -173,46 +194,191 @@ class MeshData {
       identical(this, other) ||
       (other is MeshData &&
           runtimeType == other.runtimeType &&
+          normals == other.normals &&
+          textureVertices == other.textureVertices &&
           vertices == other.vertices &&
-          faces == other.faces);
+          faces == other.faces &&
+          smooth == other.smooth);
 
   @override
-  int get hashCode => vertices.hashCode ^ faces.hashCode;
+  int get hashCode =>
+      vertices.hashCode ^
+      normals.hashCode ^
+      textureVertices.hashCode ^
+      faces.hashCode ^
+      smooth.hashCode;
 
   @override
   String toString() {
-    return 'Mesh{ vertices: $vertices, faces: $faces,}';
+    return 'Mesh{ vertices: $vertices, '
+        'normals: $normals, '
+        'textureVertices: $textureVertices, '
+        'faces: $faces, '
+        'smooth: $smooth}';
   }
 
   MeshData copyWith({
     List<Vector3>? vertices,
+    List<Vector3>? normals,
+    List<Vector3>? textureVertices,
     List<MeshFace>? faces,
+    bool? smooth,
   }) {
     return MeshData(
       vertices: vertices ?? this.vertices,
+      normals: normals ?? this.normals,
+      textureVertices: textureVertices ?? this.textureVertices,
       faces: faces ?? this.faces,
+      smooth: smooth ?? this.smooth,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
       'vertices': vertices,
+      'normals': normals,
+      'textureVertices': textureVertices,
       'faces': faces,
+      'smooth': smooth,
     };
   }
 
   factory MeshData.fromMap(Map<String, dynamic> map) {
     return MeshData(
       vertices: map['vertices'] as List<Vector3>,
+      normals: map['normals'] as List<Vector3>,
+      textureVertices: map['textureVertices'] as List<Vector3>,
       faces: map['faces'] as List<MeshFace>,
+      smooth: map['smooth'] as bool,
     );
   }
 
 //</editor-fold>
 }
 
+// Abstract mesh
+
 abstract class AbstractMesh {
-  MeshData toMeshData();
+  @protected
+  List<String> toPath(String path) => path.split('.');
+
+  MeshData toMeshData(Node rootNode);
+}
+
+//
+// Cube
+//
+
+final _cubeVertices = <Vector3>[
+  Vector3(1, 1, -1),
+  Vector3(1, -1, -1),
+  Vector3(1, 1, 1),
+  Vector3(1, -1, 1),
+  Vector3(-1, 1, -1),
+  Vector3(-1, -1, -1),
+  Vector3(-1, 1, 1),
+  Vector3(-1, -1, 1),
+];
+
+final _cubeNormals = <Vector3>[y_, z_, -x_, -y_, x_, -z_];
+
+final _cubeFaces = <MeshFace>[
+  <MeshVertex>[
+    MeshVertex(0, -1, 0),
+    MeshVertex(4, -1, 0),
+    MeshVertex(6, -1, 0),
+    MeshVertex(2, -1, 0),
+  ],
+  <MeshVertex>[
+    MeshVertex(3, -1, 1),
+    MeshVertex(2, -1, 1),
+    MeshVertex(6, -1, 1),
+    MeshVertex(7, -1, 1),
+  ],
+  <MeshVertex>[
+    MeshVertex(7, -1, 2),
+    MeshVertex(6, -1, 2),
+    MeshVertex(4, -1, 2),
+    MeshVertex(5, -1, 2),
+  ],
+  <MeshVertex>[
+    MeshVertex(5, -1, 3),
+    MeshVertex(1, -1, 3),
+    MeshVertex(3, -1, 3),
+    MeshVertex(7, -1, 3),
+  ],
+  <MeshVertex>[
+    MeshVertex(1, -1, 4),
+    MeshVertex(0, -1, 4),
+    MeshVertex(2, -1, 4),
+    MeshVertex(3, -1, 4),
+  ],
+  <MeshVertex>[
+    MeshVertex(5, -1, 5),
+    MeshVertex(4, -1, 5),
+    MeshVertex(0, -1, 5),
+    MeshVertex(1, -1, 5),
+  ],
+];
+
+MeshData _toCubeMeshData(Matrix4 matrix) {
+  return MeshData(
+    vertices: _cubeVertices,
+    normals: _cubeNormals,
+    faces: _cubeFaces,
+    smooth: false,
+  );
+}
+
+class CubeMesh extends AbstractMesh {
+  String path;
+
+  CubeMesh({
+    required this.path,
+  });
+
+  @override
+  MeshData toMeshData(Node rootNode) {
+    final matrix = rootNode.find(toPath(path));
+    return _toCubeMeshData(matrix!.value);
+  }
+
+//<editor-fold desc="Data Methods">
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      (other is CubeMesh && runtimeType == other.runtimeType && path == other.path);
+
+  @override
+  int get hashCode => path.hashCode;
+
+  @override
+  String toString() {
+    return 'CubeMesh{' ' path: $path,' '}';
+  }
+
+  CubeMesh copyWith({
+    String? path,
+  }) {
+    return CubeMesh(
+      path: path ?? this.path,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'path': path,
+    };
+  }
+
+  factory CubeMesh.fromMap(Map<String, dynamic> map) {
+    return CubeMesh(
+      path: map['path'] as String,
+    );
+  }
+
+//</editor-fold>
 }
 
 //
